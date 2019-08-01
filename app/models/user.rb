4d877ad9e9fa02dbb9e1f344d2456baa4f2cbc7a
chikugenkento <knt37007@gmail.com>
2019-08-01 17:49:44 +0000

    課題１

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,3 +7,10 @@ class User < ApplicationRecord
 
   has_secure_password
 end
+
+class User < ApplicationRecord
+   validates :password, length: { in: 8..32 }  
+
+   validates :name, presence: true,
+   length: { maximum: 15 }
+end
